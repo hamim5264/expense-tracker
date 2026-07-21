d849b8c045b99b44074651e9d9f7bdef7a4433a7
hamim5264 <hamim15-5264@diu.edu.bd>
2026-07-21 15:21:44 +0600

    chore: bump version to 1.0.1+2 and target Android SDK 35 for Google Play release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
     defaultConfig {
         applicationId = "com.thedevhamim.onyx"
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
